ffce6422d3f65e3be52d6d62c3ed014aad62c0e7
Baqer Naqvi <link2naqvi@gmail.com>
2023-05-20 01:34:47 +0500

    Initial MVC project added

---
+++ NotificationEngine/NotificationEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NotificationEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
