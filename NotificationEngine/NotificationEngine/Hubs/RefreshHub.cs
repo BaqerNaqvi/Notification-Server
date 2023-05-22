7219fc03794324ff305980900f2f95130a94a434
Baqer Naqvi <link2naqvi@gmail.com>
2023-05-23 02:29:25 +0500

    Server has a REST API working. all good React client added

---
+++ NotificationEngine/NotificationEngine/Hubs/RefreshHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NotificationEngine.Models;
+
+namespace NotificationEngine.Hubs
+{
+    public class RefreshHub : Hub
+    {
+        public async Task SendMessage(ChatMessage chatMessage)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", chatMessage);
+        }
+    }
+}
